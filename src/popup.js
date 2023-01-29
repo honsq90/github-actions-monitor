61f312ebf990af20cc905bbfbbe50798d66d1b6e
Shuqian Hon <honsq90@gmail.com>
2023-01-29 16:34:20 +0800

    Render status icon in popup

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,9 @@
 'use strict';
 
 import './popup.css';
+import greenLogo from '../public/icons/green_128.png'
+import orangeLogo from '../public/icons/orange_128.png'
+import redLogo from '../public/icons/red_128.png'
 
 (function () {
   // We will make use of Storage API to get and store `count` value
@@ -17,8 +20,29 @@ import './popup.css';
       trackList.forEach(repo => {
 
         const node = document.createElement("li");
-        const link = document.createTextNode(`${repo.owner}/${repo.name} - ${repo.status}`)
-        node.appendChild(link);
+        node.classList.add("statusItem")
+
+        const repoSpan = document.createElement("span");
+        const repoText = document.createTextNode(`${repo.owner}/${repo.name}`)
+        repoSpan.appendChild(repoText);
+        const statusIcon = document.createElement("img");
+        statusIcon.setAttribute('height', 12)
+        statusIcon.setAttribute('width', 12)
+        switch (repo.status) {
+          case "completed":
+            statusIcon.setAttribute('src', greenLogo)
+            break;
+          case "in_progress":
+            statusIcon.setAttribute('src', orangeLogo)
+            break;
+          default:
+            statusIcon.setAttribute('src', redLogo)
+            break;
+        }
+
+        node.appendChild(repoSpan)
+        node.appendChild(statusIcon)
+
         list.appendChild(node);
       })
     })
